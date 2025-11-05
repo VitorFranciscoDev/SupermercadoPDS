dee668cb3dd1f1413848cdec5e081ec355ae7b82
VitorFranciscoDev <vitorfndr@gmail.com>
2025-11-05 09:03:29 -0300

    feat: auth controller done

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,16 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:supermercado/controller/auth_controller.dart';
+import 'package:supermercado/model/userDAO.dart';
 import 'package:supermercado/view/app.dart';
 
 void main() {
+  final userDAO = UserDAO();
+
   runApp(
     MultiProvider(
       providers: [
-        ChangeNotifierProvider(create: (_) => AuthController()),
+        ChangeNotifierProvider(create: (_) => AuthController(userDAO: userDAO)),
       ],
       child: const MyApp(),
     ),
